6aa2d4fbc15dc3d63b9cd10a19cc1d0d8ae1e670
chaitanya-sai <chaitanyasaiwork@gmail.com>
2025-03-08 09:00:13 +0530

    updates

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
-    console.log(err);
+    console.log(err, 'error');
 });
 
 
